e225a8b2ecf34f6f4451a00659138aa1a5a2f5bf
grrigore <cristi9627@yahoo.com>
2017-11-25 16:06:23 +0200

    lifecycle example

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Android-Activity-Lifecycle
-Android Activity Lifecycle example
+
+Simple Android activity lifecycle example to see how the lifecycle callbacks work.
+
+* onCreate()
+* onStart()
+* onResume()
+* onPause()
+* onStop()
+* onDestroy()
---
+++ app/src/main/java/com/example/android/testapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.android.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.sql.BatchUpdateException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("onCreate ------ ", "MainActivity: onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart ------ ","MainActivity: onStart()");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume ------ ","MainActivity: onResume()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("onPause ------ ","MainActivity: onPause()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("onStop ------ ","MainActivity: onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("onDestroy ------ ","MainActivity: onDestroy()");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("onRestart ------ ","MainActivity: onRestart()");
+
+    }
+}
